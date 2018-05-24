7c9ed3cc37c455b69d37211d91f53711d40cf52b
Andrej Lamov <andrej.lamov@gmail.com>
2018-05-24 15:44:19 +0200

    Make functions interactive

--- editor-plugins/emacs/gito.el
+++ editor-plugins/emacs/gito.el
@@ -2,11 +2,13 @@
   (shell-command "gito sync"))
 
 (defun gito-enable ()
+  (interactive)
   (global-auto-revert-mode 1)
   (setq auto-revert-interval 0.5)
   (add-hook 'after-save-hook 'gito-sync))
 
 (defun gito-disable ()
+  (interactive)
   (global-auto-revert-mode -1)
-  (setq auto-revert-interval 5)
+  (setq auto-revert-interval 5) ;; default value
   (remove-hook 'after-save-hook 'gito-sync))
