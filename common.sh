d43102900edb10491619b1d176489d7197b207e1
Christopher Fredén <christopher.freden@seal-software.com>
2018-05-24 11:10:54 +0200

    Init

---
+++ common.sh
@@ -0,0 +1,34 @@
+rtrav() {
+  test -e $2/$1 && printf %s "$2" || { test $2 != / && rtrav $1 `dirname $2`; }
+}
+die() {
+  [ -n "$1" ] && echo >&2 "Error: $1"
+  exit 1
+}
+stop() {
+  [ -n "$1" ] && echo >&2 "$1"
+  exit 0
+}
+initrepo() {
+  (
+    cd "$1"
+    echo .gito/ > .gito/gitoignore
+    git config --local core.excludesfile .gito/gitoignore
+    git config --local user.email "$(whoami)@$(hostname)"
+    git config --local user.name "$(whoami)"
+  )
+}
+setrepo() {
+  export GIT_WORK_TREE="`rtrav .gito $(cd $1;pwd)`"
+  export GIT_DIR="$GIT_WORK_TREE/.gito"
+}
+
+SCRIPT_NAME="$(basename $0)"
+SCRIPT_DIR="$(cd `dirname $0`;pwd)"
+SCRIPT_USER="gito"
+SCRIPT_GROUP="gito"
+
+export PATH="$PATH:$SCRIPT_DIR"
+export GIT_CONFIG_NOSYSTEM=1
+unset XDG_CONFIG_HOME
+unset HOME
